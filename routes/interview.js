af4004b27352cb53c61e59c7cb9c8b243a923569
shao <yangshao2014@gmail.com>
2016-03-31 14:26:20 -0400

    second commit with routes/interview and app.js

---
+++ routes/interview.js
@@ -0,0 +1,10 @@
+var router = require('express').Router();
+var dbConn = require('mongodb').MongoClient.connect('mongodb://localhost/l2');
+
+router.get('/', function(req, res, next){
+    dbConn.then(function(db){
+        return db.collection('interview').find().toArray();
+    }).then(res.json.bind(res)).catch(next);
+});
+
+module.exports = router;
